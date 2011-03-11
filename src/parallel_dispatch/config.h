5a7b9816a20a438f786446ef7bf505040a367dad
planet <priv.planet@gmail.com>
2011-03-11 09:49:57 +0900

    3/11

--- src/parallel_dispatch/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace g2io
-	{
-	static const unsigned int EPOLL_EVENT_SIZE = 1024;
-	}
--- src/parallel_dispatch/g2includes.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "eventtype.h"
-#include "epolldata.h"
-#include "tcpsocket.h"
-#include "iodispatch.h"
-#include "buffer.h"
-#include "debugutility.h"
-
